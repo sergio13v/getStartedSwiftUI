b010469e70e4e7cd0827418a19bce5bdd990d5b3
sergio13v <56689687+sergio13v@users.noreply.github.com>
2019-11-28 20:49:25 +0100

    Initial Commit

---
+++ getStartedSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  getStartedSwiftUI
+//
+//  Created by Sergio Vázquez on 28/11/2019.
+//  Copyright © 2019 sergio13v. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
